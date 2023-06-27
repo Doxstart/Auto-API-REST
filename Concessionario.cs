03afcf8a6cad8297ef4d0349d11440ebd620c578
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-06-27 10:57:49 +0200

    Add project files.

---
+++ Concessionario.cs
@@ -0,0 +1,32 @@
+﻿namespace Auto_API_REST
+{
+    public class CarDealer
+    {
+
+        public int DealerId { get; set; }
+        public List<Car>? ListofCars { get; set; }
+
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public double Weight { get; set; }
+        public double Speed { get; set; }
+        public double MaxSpeed { get; set; }
+        public int Displacement { get; set; }
+
+        public Car (int id, string brand, double weight, double speed, double maxSpeed, int displacement)
+        {
+            Id = id;
+            Brand = brand;
+            Weight = weight;
+            Speed = speed;
+            MaxSpeed = maxSpeed;
+            Displacement = displacement;
+        }
+
+    }
+
+}
---
+++ Controllers/ConcessionarioController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Auto_API_REST;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auto_API_REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConcessionarioController : ControllerBase
+    {
+        private static List<CarDealer> carDealers = new List<CarDealer>();
+
+        // GET: api/CarDealer
+        [HttpGet]
+        public ActionResult<IEnumerable<CarDealer>> Get()
+        {
+            return carDealers;
+        }
+
+        // GET api/CarDealer/5
+        [HttpGet("{id}")]
+        public ActionResult<CarDealer> Get(int id)
+        {
+            var carDealer = carDealers.FirstOrDefault(d => d.DealerId == id);
+            if (carDealer == null)
+            {
+                return NotFound();
+            }
+            return carDealer;
+        }
+
+        // POST api/CarDealer
+        [HttpPost]
+        public ActionResult<CarDealer> Post(CarDealer carDealer)
+        {
+            carDealers.Add(carDealer);
+            return CreatedAtAction(nameof(Get), new { id = carDealer.DealerId }, carDealer);
+        }
+
+        // PUT api/<ConcessionarioController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/CarDealer/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            foreach (var carDealer in carDealers)
+            {
+                var carToRemove = carDealer.ListofCars?.FirstOrDefault(car => car.Id == id);
+
+                if (carToRemove != null)
+                {
+                    carDealer.ListofCars?.Remove(carToRemove);
+                    return NoContent();
+                }
+            }
+            return NotFound();
+        }
+    }
+}
